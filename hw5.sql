568c88e3065a4584d28fac78518c78d35e5c48f7
syuksel <samet.yuksel.50@gmail.com>
2022-08-02 14:31:14 +0300

    Added HW5
    ORDER BY, LIMIT ve OFFSET keyword leri için sorular cevaplandırıldı.

---
+++ hw5.sql
@@ -0,0 +1,17 @@
+--18) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+OFFSET 5;
+
+--19) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY ASC
+OFFSET 5;
+
+--20) customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+SELECT last_name FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
