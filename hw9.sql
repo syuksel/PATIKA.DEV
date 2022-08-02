aebf5fc19c78cf68cc65cdbedc279e1fc770f756
syuksel <samet.yuksel.50@gmail.com>
2022-08-02 16:14:45 +0300

    HW9 Added
    Used INNER JOIN

--- hw9.sql
+++ hw9.sql
@@ -1,17 +1,17 @@
---36) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
-SELECT  CI.name, CO.name
+--33) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT CI.name, CO.name
 FROM CITY CI
-LEFT JOIN COUNTRY CO
+INNER JOIN COUNTRY CO
 ON CI.country_id = CO.id;
 
---37) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
-SELECT P.id, C.first_name, P.last_name
-FROM COSTUMER C
-RIGHT JOIN PAYMENT P
-ON C.payment_id = P.id;
+--34) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT C.payment_id, P.payment_id, C.first_name, C.last_name
+FROM CUSTOMER C
+INNER JOIN PAYMENT P
+ON C.id = P.customer_id;
 
---38) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+--35) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 SELECT R.id, C.first_name, C.last_name
 FROM CUSTOMER C
-FULL JOIN RENTAL R
+INNER JOIN RENTAL R
 ON C.rental_id = R.id;
